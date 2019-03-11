31a5a658a2ad4eba875a8ab14df12fd568034b84
Vitālijs Gaičuks <vitalijs.gaicuks@gmail.com>
2019-03-11 16:14:53 +0200

    Dpass ERC-721 contract

--- src/Dpass.sol
+++ src/Dpass.sol
@@ -1,4 +1,78 @@
 pragma solidity ^0.5.4;
 
-contract Dpass {
+// /**
+//  * How to use dapp and openzeppelin-solidity https://github.com/dapphub/dapp/issues/70
+//  * ERC-721 standart: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md
+//  *
+//  */
+
+import "openzeppelin-solidity/token/ERC721/ERC721Full.sol";
+
+
+contract Dpass is ERC721Full {
+    string private _name = "Diamond Passport (Dpass)";
+    string private _symbol = "DP";
+
+    struct Diamond {
+        string gia;
+        uint carat_weight;
+    }
+
+    Diamond[] diamonds;
+
+    constructor () public ERC721Full(_name, _symbol) {
+        // pass
+    }
+
+    /**
+    * @dev Custom accessor to create a unique token
+    * @param _to address of diamond owner
+    * @param _gia string diamond GIA agency unique Nr.
+    * @param _carat_weight uint diamond carat weight
+    * @return Return Diamond tokenId of the diamonds list
+    */
+    function mintDiamondTo(address _to, string memory _gia, uint _carat_weight) public {
+        uint256 _tokenId = _createDiamond(_gia, _carat_weight);
+
+        super._mint(_to, _tokenId);
+        super._setTokenURI(_tokenId);
+    }
+
+    function _createDiamond(string memory _gia, uint _carat_weight) internal returns (uint) {
+        Diamond memory _diamond = Diamond({
+            gia: _gia,
+            carat_weight: _carat_weight
+        });
+
+        uint256 newDiamondId = diamonds.push(_diamond) - 1;
+
+        return newDiamondId;
+    }
+
+    /**
+     * @dev Gets the Diamond at a given _tokenId of all the diamonds in this contract
+     * Reverts if the _tokenId is greater or equal to the total number of diamonds
+     * @param _tokenId uint256 representing the index to be accessed of the diamonds list
+     * @return Returns all the relevant information about a specific diamond
+     */
+    function getDiamond(uint256 _tokenId) public view returns(string memory gia, uint carat_weight) {
+        require(_tokenId < totalSupply(), "Diamond does not exist");
+
+        Diamond storage _diamond = diamonds[_tokenId];
+        gia = _diamond.gia;
+        carat_weight = _diamond.carat_weight;
+    }
+
+    /**
+     * @dev Gets the Diamond gia number at a given _tokenId of all the diamonds in this contract
+     * Reverts if the _tokenId is greater or equal to the total number of diamonds
+     * @param _tokenId uint256 representing the index to be accessed of the diamonds list
+     * @return Gia information about a specific diamond
+     */
+    function diamondCaratByIndex(uint256 _tokenId) public view returns (uint) {
+        require(_tokenId < totalSupply(), "Diamond does not exist");
+
+        Diamond storage _diamond = diamonds[_tokenId];
+        return _diamond.gia;
+    }
 }
--- src/Dpass.t.sol
+++ src/Dpass.t.sol
@@ -4,11 +4,21 @@ import "ds-test/test.sol";
 
 import "./Dpass.sol";
 
+contract DpassTester {
+    Dpass public _dpass;
+
+    constructor(Dpass dpass) public {
+        _dpass = dpass;
+    }
+}
+
 contract DpassTest is DSTest {
     Dpass dpass;
+    DpassTester user;
 
     function setUp() public {
         dpass = new Dpass();
+        user = new DpassTester(dpass);
     }
 
     function testFail_basic_sanity() public {
@@ -17,5 +27,21 @@ contract DpassTest is DSTest {
 
     function test_basic_sanity() public {
         assertTrue(true);
+    }
+
+    // function test_symbol_func() public {
+    //     // dpass.symbol();
+    //     string memory r = dpass.symbol();
+    //     assertEq0(r, "DP");
+    // }
+
+    function test_diamond_balance() public {
+        dpass.mintDiamondTo(address(user), "7296159262", 710000000000000000, "");
+        assertEq(dpass.balanceOf(address(user)), 1);
+    }
+
+    function test_diamond_cart_weight() public {
+        dpass.mintDiamondTo(address(user), "7296159262", 710000000000000000, "");
+        assertEq(dpass.diamondCaratByIndex(0), 710000000000000000);
     }
 }
