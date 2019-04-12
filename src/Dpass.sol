8d50dfcbc1656b71cfb24d1a801a7adefedddfa8
Vitālijs Gaičuks <vitalijs.gaicuks@gmail.com>
2019-04-12 09:06:23 +0300

    Sale status

--- src/Dpass.sol
+++ src/Dpass.sol
@@ -16,12 +16,18 @@ contract DpassEvents {
         uint price
     );
 
+    event LogSaleStatusChanged(
+        uint token_id,
+        bool sale
+    );
+
     event LogDiamondMinted(
         address owner,
         uint token_id,
         string gia,
         uint carat_weight,
-        uint price
+        uint price,
+        bool sale
     );
 }
 
@@ -34,6 +40,7 @@ contract Dpass is DSAuth, ERC721Full, DpassEvents {
         string gia;
         uint carat_weight;
         uint price;
+        bool sale;
     }
 
     Diamond[] diamonds;
@@ -48,20 +55,22 @@ contract Dpass is DSAuth, ERC721Full, DpassEvents {
     * @param _gia string diamond GIA agency unique Nr.
     * @param _carat_weight uint diamond carat weight (4 decimals) format 10**4 (ex 0.71 carat is 71000)
     * @param _price uint diamond price
+    * @param _sale bool is diamond can be purched
     * @return Return Diamond tokenId of the diamonds list
     */
-    function mintDiamondTo(address _to, string memory _gia, uint _carat_weight, uint _price) public auth {
-        uint256 _tokenId = _createDiamond(_gia, _carat_weight, _price);
+    function mintDiamondTo(address _to, string memory _gia, uint _carat_weight, uint _price, bool _sale) public auth {
+        uint256 _tokenId = _createDiamond(_gia, _carat_weight, _price, _sale);
 
         super._mint(_to, _tokenId);
-        emit LogDiamondMinted(_to, _tokenId, _gia, _carat_weight, _price);
+        emit LogDiamondMinted(_to, _tokenId, _gia, _carat_weight, _price, _sale);
     }
 
-    function _createDiamond(string memory _gia, uint _carat_weight, uint _price) internal returns (uint) {
+    function _createDiamond(string memory _gia, uint _carat_weight, uint _price, bool _sale) internal returns (uint) {
         Diamond memory _diamond = Diamond({
             gia: _gia,
             carat_weight: _carat_weight,
-            price: _price
+            price: _price,
+            sale: _sale
         });
 
         uint256 newDiamondId = diamonds.push(_diamond) - 1;
@@ -75,13 +84,14 @@ contract Dpass is DSAuth, ERC721Full, DpassEvents {
      * @param _tokenId uint256 representing the index to be accessed of the diamonds list
      * @return Returns all the relevant information about a specific diamond
      */
-    function getDiamond(uint256 _tokenId) public view returns (string memory gia, uint carat_weight, uint price) {
+    function getDiamond(uint256 _tokenId) public view returns (string memory gia, uint carat_weight, uint price, bool sale) {
         require(_tokenId < totalSupply(), "Diamond does not exist");
 
         Diamond storage _diamond = diamonds[_tokenId];
         gia = _diamond.gia;
         carat_weight = _diamond.carat_weight;
         price = _diamond.price;
+        sale = _diamond.sale;
     }
 
     /**
@@ -126,6 +136,25 @@ contract Dpass is DSAuth, ERC721Full, DpassEvents {
 
         if (old_price != _price) {
             emit LogPriceChanged(_tokenId, _price);
+        }
+    }
+
+    /**
+     * @dev Set Diamond sale status
+     * Reverts if the _tokenId is greater or equal to the total number of diamonds
+     * @param _tokenId uint256 representing the index to be accessed of the diamonds list
+     * @param _sale bool new diamond sale status
+     */
+    function setSaleStatus(uint256 _tokenId, bool _sale) public {
+        require(ownerOf(_tokenId) == msg.sender, "Access denied");
+        require(_tokenId < totalSupply(), "Diamond does not exist");
+
+        Diamond storage _diamond = diamonds[_tokenId];
+        bool old_sale = _diamond.sale;
+        _diamond.sale = _sale;
+
+        if (old_sale != _sale) {
+            emit LogSaleStatusChanged(_tokenId, _sale);
         }
     }
 }
--- src/Dpass.t.sol
+++ src/Dpass.t.sol
@@ -14,6 +14,10 @@ contract DpassTester {
     function doSetPrice(uint256 token_id, uint price) public {
         _dpass.setPrice(token_id, price);
     }
+
+    function doSetSaleStatus(uint256 token_id, bool sale) public {
+        _dpass.setSaleStatus(token_id, sale);
+    }
 }
 
 contract DpassTest is DSTest {
@@ -38,28 +42,39 @@ contract DpassTest is DSTest {
     }
 
     function test_diamond_balance() public {
-        dpass.mintDiamondTo(address(user), "GIA1", 7100, 1 ether);
+        dpass.mintDiamondTo(address(user), "GIA1", 7100, 1 ether, true);
         assertEq(dpass.balanceOf(address(user)), 1);
     }
 
     function test_diamond_gia() public {
-        dpass.mintDiamondTo(address(user), "GIA1", 7100, 1 ether);
+        dpass.mintDiamondTo(address(user), "GIA1", 7100, 1 ether, true);
         assertEq0(bytes(dpass.getDiamondGia(0)), bytes("GIA1"));
     }
 
     function testFail_non_owner() public {
         dpass.setOwner(address(0));
-        dpass.mintDiamondTo(address(user), "GIA1", 7100, 1 ether);
+        dpass.mintDiamondTo(address(user), "GIA1", 7100, 1 ether, true);
     }
 
     function test_ownership_of_new_diamond() public {
-        dpass.mintDiamondTo(address(user), "GIA1", 7100, 1 ether);
+        dpass.mintDiamondTo(address(user), "GIA1", 7100, 1 ether, true);
         assertEq(dpass.ownerOf(0), address(user));
     }
 
     function test_price_change() public {
-        dpass.mintDiamondTo(address(user), "GIA1", 7100, 1 ether);
+        dpass.mintDiamondTo(address(user), "GIA1", 7100, 1 ether, true);
         user.doSetPrice(0, 2 ether);
         assertEq(dpass.getPrice(0), 2 ether);
+    }
+
+    function test_sale_status_change() public {
+        dpass.mintDiamondTo(address(user), "GIA1", 7100, 1 ether, true);
+        user.doSetSaleStatus(0, false);
+        string memory gia;
+        uint256 carat;
+        uint256 price;
+        bool sale;
+        (gia, carat, price, sale) = dpass.getDiamond(0);
+        assertTrue(!sale);
     }
 }
